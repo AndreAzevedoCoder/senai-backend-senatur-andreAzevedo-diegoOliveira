58b258f0cf0f2f9503b32eea11bd00a38d7f5569
André Santos De Azevedo <54094513841@info.local>
2020-03-10 10:19:26 -0300

    colocando ramo

---
+++ BACK-END/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Controllers/PacotesController - Cópia.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Senatur.WebApi.Domains;
+using Senai.Senatur.WebApi.Interfaces;
+using Senai.Senatur.WebApi.Repositories;
+
+namespace Senai.Senatur.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class PacotesController : ControllerBase
+    {
+        private IPacotesRepository _pacotesRepository { get; set; }
+
+        public PacotesController()
+        {
+            _pacotesRepository = new PacoteRepository();
+        }
+
+        [HttpGet]
+        public IActionResult ListarTodos()
+        {
+            return StatusCode(200, _pacotesRepository.Listar());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ListarTodos(int id)
+        {
+            return StatusCode(200, _pacotesRepository.BuscarPorID(id));
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Pacotes pacote)
+        {
+            _pacotesRepository.Cadastrar(pacote);
+            return StatusCode(200);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Pacotes pacote)
+        {
+            _pacotesRepository.Atualizar(id,pacote);
+            return StatusCode(200);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            _pacotesRepository.Deletar(id);
+            return StatusCode(200);
+        }
+
+    }
+}
